b507ee49a8220fc10152e1e9dccfd2df59e5321f
theva01 <Oeduardo263@gmail.com>
2024-04-05 22:20:30 -0500

    cap #39

--- PracticaPoo1/Form1.cs
+++ PracticaPoo1/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Productos;
 
 namespace PracticaPoo1
 {
     public partial class Form1 : Form
     {
+        Telefono Movistar = new Telefono();//Se creo el objeto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLlamar_Click(object sender, EventArgs e)
+        {
+            Movistar.llamar();
+            //Movistar.marca = "Samsung";
+            Movistar.llamar();
         }
     }
 }
---
+++ PracticaPoo1/Productos/Telefono.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+
+
+    public class Telefono
+    {
+        private string marca;
+        private string color;
+        private string tipo;
+
+        public Telefono()//ctor se encarga de inicializar los atributos
+        {
+            marca = "Apple";
+            color = "azul";
+            tipo = "touch";
+            //este metodo no retorna datos
+        }//solo se va a ejecutar una vez
+
+        public void llamar()
+        {
+            System.Windows.Forms.MessageBox.Show("MI celular es un: "+ marca);
+        }
+
+        public void MandarMensaje()
+        {
+
+        }
+
+    }
+}
